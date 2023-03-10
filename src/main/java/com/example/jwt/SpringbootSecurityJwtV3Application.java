484e7de84e2d1cd3ade50ed16a9bcb9f8224a37b
An T.G <boltcarrier@naver.com>
2023-03-11 05:33:35 +0900

    first commit

---
+++ src/main/java/com/example/jwt/SpringbootSecurityJwtV3Application.java
@@ -0,0 +1,13 @@
+package com.example.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityJwtV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityJwtV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwt/SpringbootSecurityJwtV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityJwtV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
